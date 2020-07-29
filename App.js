9b6c32cf11c88ae225f4739e948d792b633e196f
Aandrej2 <andrej.zlamala@revolware.com>
2020-07-29 23:55:43 +0200

    Add expo document picker and test button
    Signed-off-by: Aandrej2 <andrej.zlamala@revolware.com>

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import * as DocumentPicker from 'expo-document-picker';
+
+
+export default class App extends React.Component {
+
+  clicked = (event) => {
+    console.log("AAA");
+    DocumentPicker.getDocumentAsync({
+      type: "*/*",
+      copyToCacheDirectory: true,
+      multiple: false
+    }).then((e) => {
+      console.log("Result: ", e);
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hi Thomas!</Text>
+        <StatusBar style="auto"/>
+        <Button onPress={this.clicked} title="Test">File Input</Button>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({

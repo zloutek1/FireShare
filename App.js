c30de7ae859e04a60c025f7e639f35845cf23898
Aandrej2 <andrej.zlamala@revolware.com>
2020-07-30 16:54:25 +0200

    Add Expo FileSystem and Image preview
    Signed-off-by: Aandrej2 <andrej.zlamala@revolware.com>

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 
 import MultiTap from './MultiTap'
 import * as DocumentPicker from 'expo-document-picker';
+import * as FileSystem from 'expo-file-system';
 const background_parcel = require('./assets/background_parcel_trans.png');
 
 
@@ -14,46 +15,79 @@ export default class App extends React.Component {
   }
 
   clicked = (event) => {
-    console.log("AAA");
     DocumentPicker.getDocumentAsync({
-      type: "*/*",
-      copyToCacheDirectory: false,
+      type: "image/*",
+      copyToCacheDirectory: true,
       multiple: true
     }).then((e) => {
       if(e.type === 'success' && e.uri) {
-        console.log("File: ", e);
-        this.state.files.push(e);
-        this.setState({
-          files: this.state.files,
+        FileSystem.readAsStringAsync(e.uri, {
+          encoding: FileSystem.EncodingType.Base64
+        })
+          .then((data) => {
+            this.state.files.push({
+              uri: 'data:image/png;base64,'+data,
+              name: e.name,
+            });
+            this.setState({
+              files: this.state.files,
+            })
+        }).catch(error => {
+          console.error(error);
         })
       }
     })
   }
 
+  imageSize = (x) => {
+    let width = 0;
+    let height = 0;
+
+    if(x < 5) {
+      width = height = 46;
+    } else if(x < 10) {
+      width = height = 30;
+    } else if(x < 17) {
+      width = height = 22;
+    } else if(x < 26) {
+      width = height = 17;
+    } else if(x < 37) {
+      width = height = 13;
+    } else {
+      width = height = 100;
+    }
+
+    return {
+      width: width+'%',
+      height: height+'%',
+    }
+  }
+
   render() {
     const {files} = this.state
+
+    const imageStyle = this.imageSize(files.length);
 
     return (
         <View style={styles.container}>
           <MultiTap onPress={this.clicked}>
             <Image source={background_parcel} style={styles.backgroundImage} backgroundColor={"#10101F0A"} opacity={0.5} />
             <View style={styles.app}>
-              <Text>Use two fingers to tap the screen!</Text>
-              <StatusBar style="auto"/>
-              <View>
-                {files.length > 0 && (
-                  <View>
-                    <Text>{"\n"}Uploaded files:</Text>
-                  </View>
-                )}
-                {files.map((item) => {
-                  return (
-                    <View key={`file-`+item.name}>
-                      <Text>{item.name}</Text>
-                    </View>
-                  )
-                })}
-              </View>
+              <StatusBar style="auto" hidden={true}/>
+              {files.length === 0 && (
+                <Text>Use two fingers to tap the screen!</Text>
+              )}
+              {files.length > 0 && (
+                <View style={[styles.imagePreview, files.length === 1 ? styles.imagePreviewSingle : styles.imagePreviewMultiple ]}>
+                  {files.map((item, key) => {
+                    return (
+                      <View key={`file-` + item.name + '-' + key} style={[styles.imageBox, imageStyle]}>
+                        <Image source={{'uri': item.uri}} style={{width:'100%', height:'100%'}} />
+                      </View>
+                    )
+                  })}
+                </View>
+              )}
             </View>
           </MultiTap>
         </View>
@@ -83,7 +117,26 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   imagePreview: {
-    flex: 1,
-    width: '10px'
+    display: 'flex',
+    flexDirection: 'row',
+    position: 'relative',
+
+    width: '95%',
+    height: '95%',
+    flexWrap: 'wrap',
+  },
+  imagePreviewSingle: {
+    alignItems: 'center',
+    alignContent: 'center',
+    justifyContent: 'center'
+  },
+  imagePreviewMultiple: {
+    alignItems: 'stretch',
+    alignContent: 'space-around',
+    justifyContent: 'space-around'
+  },
+  imageBox: {
+    display: 'flex',
+    position: 'relative',
   }
 });

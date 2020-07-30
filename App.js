5defd119e08d6e9a6359b5608261c871aa027c29
Aandrej2 <andrej.zlamala@revolware.com>
2020-07-30 12:56:48 +0200

    Add fancy tiled background
    Signed-off-by: Aandrej2 <andrej.zlamala@revolware.com>

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 import * as DocumentPicker from 'expo-document-picker';
+const background_parcel = require('./assets/background_parcel_trans.png');
 
 
 export default class App extends React.Component {
@@ -20,11 +21,14 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hi Thomas!</Text>
-        <StatusBar style="auto"/>
-        <Button onPress={this.clicked} title="Test">File Input</Button>
-      </View>
+        <View style={styles.container}>
+            <Image source={background_parcel} style={styles.backgroundImage} backgroundColor={"#10101F0A"} opacity={0.5} />
+            <View style={styles.app}>
+              <Text>Hi Thomas!</Text>
+              <StatusBar style="auto"/>
+              <Button onPress={this.clicked} title="Test">File Input</Button>
+            </View>
+        </View>
     );
   }
 }
@@ -33,7 +37,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'repeat',
+    width: '100%',
+  },
+  app: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
